24d8372b77318a3334775cff757aa0efbeafc502
Kater <Katerstechhub@gmail.com>
2026-06-30 00:09:56 +0100

    aad

--- mediahub/vite.config.js
+++ mediahub/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(), // Add this for Tailwind v4
+  ],
+  server: {
+    port: 5173,
+  },
 })
