356e5d18f5982239cdd21a3c8c76adb897ccb920
Kater <Katerstechhub@gmail.com>
2026-08-23 11:00:52 +0100

    gallery

--- mediahub/src/pages/FeedPage.jsx
+++ mediahub/src/pages/FeedPage.jsx
@@ -20,7 +20,7 @@ dayjs.extend(relativeTime)
 
 import gallery1 from '../assets/gallery-1.jpeg'
 import gallery2 from '../assets/gallery-2.jpeg'
-import gallery3 from '../assets/gallery-3.jpeg'
+// import gallery3 from '../assets/gallery-3.jpeg'
 import gallery4 from '../assets/gallery-4.jpeg'
 import gallery5 from '../assets/gallery-5.jpeg'
 import pamsSolo from '../assets/pams-solo.jpeg'
